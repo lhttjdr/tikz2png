1629e4e9968a8626f7a5ece59bf6457dd564ef71
lhttjdr <lhttjdr@gmail.com>
2017-03-28 13:44:39 +0900

    first commit: bash script & some examples

---
+++ tex2png.sh
@@ -0,0 +1,28 @@
+file_name=$2
+name=${file_name%.*}
+
+case $1 in
+  create)
+      mkdir $name
+      cp template/default.tex $name/$name.tex
+      ;;
+  build)
+      cd $name
+      pdflatex $name.tex
+      pdftops -eps $name.pdf
+      convert -density 300 -transparent white $name.eps $name.png
+      ;;
+  view)
+      cd $name
+      xdg-open $name.png
+      ;;
+  delete)
+      rm -rf $name
+      ;;
+  clear)
+      cd $name
+      find . -type f -not \( -name '*.png' -or -name '*.tex' -or -name '*.sh' \) -delete
+      ;;
+  *)
+      echo "tex2png.sh create|build|view|delete|clear project_name"
+esac
